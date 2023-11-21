654cb725c38f6d2ba717f52f89be9b725a591429
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2023-11-22 01:18:34 +0800

    Simplify things

--- MultiSIngleButton.ino
+++ MultiSIngleButton.ino
@@ -7,9 +7,9 @@ unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 bool buttonState = HIGH;             // saving state of the switch
 byte tapCounter;                     // for saving no. of times the switch is pressed
-int timediff;                        //for saving the time in between each press and release of the switch
-bool flag1, flag2;                   //just two variables
-long double presstime, releasetime;  //for saving millis at press and millis at release
+int timediff;                        // for saving the time in between each press and release of the switch
+bool flag1, flag2;                   // just two variables
+long double presstime, releasetime;  // for saving millis at press and millis at release
 
 
 void setup() {
@@ -46,22 +46,22 @@ void loop() {
     presstime = millis(); //time from millis fn will save to presstime variable
     flag1 = 0;
     flag2 = 1;
-    tapCounter++; //tap counter will increase by 1
-    //delay(10); //for avoiding debouncing of the switch
+    tapCounter++; // tap counter will increase by 1
+    //delay(10); // for avoiding debouncing of the switch
   }
   //when sw is released
   if (buttonState == 1 && flag1 == 0)
   {
-    releasetime = millis(); //time from millis fn will be saved to releasetime var
+    releasetime = millis(); // time from millis fn will be saved to releasetime var
     flag1 = 1;
     flag2 = 0;
 
-    timediff = releasetime - presstime; //here we find the time gap between press and release and stored to timediff var
+    timediff = releasetime - presstime; // here we find the time gap between press and release and stored to timediff var
     //Serial.println(timediff);
     //delay(10);
   }
 
-  if ((millis() - presstime) > 400 && buttonState == 1) //wait for some time and if sw is in release position
+  if ((millis() - presstime) > 400 && buttonState == 1) // wait for some time and if CL is in release position
   {
      if (tapCounter == 4) //if tapcounter is 4 then its 4 tap
     {
